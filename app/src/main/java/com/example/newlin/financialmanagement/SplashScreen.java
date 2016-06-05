ac4902a6a595ca69cc9564fb1c3092205f0acf89
chrisgenao <christopher_genao@hotmail.com>
2016-06-04 23:39:35 -0400

    Work work work work work work!

---
+++ app/src/main/java/com/example/newlin/financialmanagement/SplashScreen.java
@@ -0,0 +1,48 @@
+package com.example.newlin.financialmanagement;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ProgressBar;
+
+public class SplashScreen extends AppCompatActivity {
+    public static final int segundos = 8;
+    public static final int milisegundos = segundos * 1000;
+    public static final int delay = 2;
+    private ProgressBar pbprogreso;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        pbprogreso = (ProgressBar) findViewById(R.id.progressBar);
+        pbprogreso.setMax(maximo_progreso());
+        empezaranimacion();
+    }
+
+    public void empezaranimacion(){
+        new CountDownTimer(milisegundos, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                pbprogreso.setProgress(establecer_progreso((millisUntilFinished)));
+            }
+
+            @Override
+            public void onFinish() {
+                Intent nuevoform = new Intent(SplashScreen.this, Main2Activity.class);
+                startActivity(nuevoform);
+            }
+        }.start();
+    }
+
+    public int establecer_progreso(long miliseconds){
+        return (int) ((milisegundos-miliseconds)/1000);
+    }
+
+    public int maximo_progreso()
+    {
+      return segundos-delay;
+    }
+}
